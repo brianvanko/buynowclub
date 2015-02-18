4999efa3bc41e05728f2e0400d72f122b1941d70
Brian Vanko <brianvanko@gmail.com>
2015-02-17 23:41:48 -0800

    test

--- api/public/scripts/app.config.js
+++ api/public/scripts/app.config.js
@@ -97,7 +97,7 @@ angular
     $httpProvider.interceptors.push('authInterceptor');
   })
 
-.constant('API_URL', 'http://localhost:3000/') //'https://buynowclub.herokuapp.com/') 
+.constant('API_URL', 'https://buynowclub.herokuapp.com/') //'http://localhost:3000/') 
 
 .run(function ($window) {
   var params = $window.location.search.substring(1);
