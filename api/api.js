b7b15e94602740ace65c808e36e4f85772eb0256
Brian Vanko <brianvanko@gmail.com>
2015-02-17 21:32:40 -0800

    moving folders

--- api/api.js
+++ api/api.js
@@ -18,13 +18,15 @@ app.use(morgan('dev'));
 ////// CORS //////
 app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Origin', 'https://dl.dropboxusercontent.com');
+	res.header('Access-Control-Allow-Origin', 'https://buynowclub.herokuapp.com');
 	res.header('Access-Control-Allow-Credentials', 'true');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
 
 	next();
 });
+
+app.use(express.static(__dirname + '/frontend/app'));
 
 ////// CONNECT TO DB //////
 mongoose.connect(config.DB_URL);
@@ -35,6 +37,11 @@ userRoutes = require('./user/routes/user.routes')(app, express);
 
 
 app.use('/items', itemRoutes);
+
+// CATCH ALL AND DEFAULT INDEX
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/frontend/app/index.html'));
+});
 
 ////// CREATE SERVER //////
 var server = app.listen(port, function () {
